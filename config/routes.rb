e3e7464ae2c75f5b7a7a57159a8213bbfea8381e
noumanshah436 <noumanshah436@gmail.com>
2022-08-31 15:52:19 +0500

    Rails 5 Routing Basics completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   get 'products', to: 'products#index', as: 'products'
 # ^verb  ^URI          ^controller#action    ^ refer: products_path
 
-
   get 'products/:id', to: 'products#show',  as: 'product' , id: /\d+/
   # ^verb  ^URI          ^controller  ^action    ^ refer: product_path, constraint (for numeric values only)
 
